a7d4b60d6489200f35c2aa5a147d2623e26217d8
Timothy Kemmis <kemmojr@gmail.com>
2022-06-30 00:09:37 +1000

    Added git ignore

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { jsx } from 'jsx';
 import './App.css';
 import axios from 'axios';
 import { Button, Container, Card, Row } from 'react-bootstrap'
